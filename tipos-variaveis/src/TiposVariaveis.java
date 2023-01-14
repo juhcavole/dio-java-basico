091d344299da195585925a22fafe199646c01481
Juhcavole <ju_rigui@hotmail.com>
2023-01-14 21:42:36 +0000

    exemplos de exceções

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //double representração milhar
+        double salarioMinimo = 2500;
+
+        //Java fortemente tipado
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Constantes
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_PI = 3.14;
+
+
     }
 }
