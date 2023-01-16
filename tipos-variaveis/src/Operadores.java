b18405639ba9ab62291cf6912fe0512b724e9e14
Juhcavole <ju_rigui@hotmail.com>
2023-01-16 20:57:12 +0000

    Lógicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -5,6 +5,12 @@ public class Operadores {
         //unários
         int numero = 5;
         boolean variavel = true;
+        int a,b;
+        a = 6;
+        b = 6;
+        String nome1= "Juliana";
+        String nome2= new String("Juliana");
+
 
         numero = -numero;
 
@@ -24,6 +30,28 @@ public class Operadores {
         //Negação
         System.out.println(!variavel);
 
+        //ternário
+        String resultado = a==b ? "verdadeiro" : "falso" ;
         
+        System.out.println(resultado);
+
+        //Relacionais
+        //equals compara conteúdos
+        System.out.println(nome1.equals(nome2));
+
+        //Lógicos
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && (7>4) ){
+            System.out.println("As duas condições são verdadeiras");
+
+        }
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
+        
+
+
     }
 }
